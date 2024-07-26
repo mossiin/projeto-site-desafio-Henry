04802a0a398290e5e06c9e87c8adc20dff1e7d53
Lucas <zdarkheadbr@gmail.com>
2024-07-26 10:41:17 -0300

    atualização

--- back.js
+++ back.js
@@ -1,7 +1,22 @@
-function openNav(){
-    alert('test')
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
 }
 
-function closeNav(){
-    alert('test2')
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
 }
+
+
+document.getElementById("hub-login").addEventListener("click", function(){
+  var login = document.getElementById("hub-login");
+  if(menu.style.display === 'none' || menu.style.display === "");{
+    menu.style.display = "block";
+    this.textContent = "HiddenMenu"
+  }
+  
+    else{
+      menu.style.display ="none";
+      this.textContent = "Mostrar Menu";
+    }
+})
+
